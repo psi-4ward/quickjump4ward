8f5d100a9d47ee0f337d616a4412e59827a88bee
Christoph Wiechert <wio@psitrax.de>
2012-09-02 17:51:08 +0200

    Fixed: Warning when no autoType is set

--- Quickjump4ward.php
+++ Quickjump4ward.php
@@ -37,7 +37,7 @@ class Quickjump4ward extends Backend {
 		if($type !='all') $this->limitEach *= 3;
 
 		$autoTypes = $this->User->quickjump4ward;
-		if(!is_array($autoTypes)) $autoTypes == array();
+		if(!is_array($autoTypes)) $autoTypes = array();
 
 		if($type == 'page' || ($type =='all' && in_array('page',$autoTypes)))
 			$this->addPages($s);
