da3ef55626b1883a136329ad2cb792e54f6e2a0e
Christoph Wiechert <wio@psitrax.de>
2014-02-03 16:00:05 +0100

    Dont inject quickjump within composer module

--- config/config.php
+++ config/config.php
@@ -2,19 +2,18 @@
 
 /**
  * Quickjump4ward
- * A Contao-Extension to quickly access cartain backend-modules
- * through typing
+ * Quickly jump to various contao pages and settings
  *
- * @copyright  4ward.media 2012 <http://www.4wardmedia.de>
- * @author     Christoph Wiechert <christoph.wiechert@4wardmedia.de>
+ * @copyright  4ward.media 2014 <http://www.4wardmedia.de>
+ * @author     Christoph Wiechert <wio@psitrax.de>
  * @package    quickjump4ward
- * @license    LGPL 
+ * @license    LGPL
  * @filesource
  */
 
 
 // Insert javascripts
-if(TL_MODE == 'BE' && \Input::get('do') != 'repository_manager')
+if(TL_MODE == 'BE' && \Input::get('do') != 'repository_manager' && \Input::get('do') != 'composer')
 {
-	$GLOBALS['TL_HOOKS']['getUserNavigation'][] = array('\Quickjump4ward\Quickjump4ward','injectJavascript');
+	$GLOBALS['TL_HOOKS']['getUserNavigation'][] = array('\Quickjump4ward\Quickjump4ward', 'injectJavascript');
 }
